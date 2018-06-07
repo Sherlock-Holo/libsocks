d981b57e13b7978ef9ad153ce323eb89bb295151
Sherlock Holo <sherlockya@gmail.com>
2018-06-07 10:44:17 +0800

    Add TestDecodeFrom

---
+++ address_test.go
@@ -0,0 +1,50 @@
+package libsocks
+
+import (
+    "testing"
+    "net"
+    "bytes"
+    "fmt"
+)
+
+func TestDecodeFrom(t *testing.T) {
+    address1 := Address{
+        Type: 1,
+        IP:   net.IP{127, 0, 0, 1},
+        Port: 80,
+    }
+
+    address3 := Address{
+        Type: 3,
+        Host: "www.qq.com",
+        Port: 80,
+    }
+
+    address4 := Address{
+        Type: 4,
+        IP:   net.IPv6loopback,
+        Port: 80,
+    }
+
+    reader1 := bytes.NewReader(address1.Bytes())
+    reader3 := bytes.NewReader(address3.Bytes())
+    reader4 := bytes.NewReader(address4.Bytes())
+
+    r1, err := DecodeFrom(reader1)
+    if err != nil {
+        t.Error(err)
+    }
+    fmt.Println(r1)
+
+    r3, err := DecodeFrom(reader3)
+    if err != nil {
+        t.Error(err)
+    }
+    fmt.Println(r3)
+
+    r4, err := DecodeFrom(reader4)
+    if err != nil {
+        t.Error(err)
+    }
+    fmt.Println(r4)
+}
